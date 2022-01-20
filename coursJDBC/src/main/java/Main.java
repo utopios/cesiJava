21205999f3b397758655d477f6ab12e79e5ea86f
ihab abadi <utopios@imac-de-ihab.home>
2022-01-20 10:21:08 +0100

    suite cours jdbc

--- coursJDBC/src/main/java/Main.java
+++ coursJDBC/src/main/java/Main.java
@@ -1,14 +1,25 @@
+import dao.ContactDAO;
+import models.Contact;
 import tools.DataConnection;
 
 import java.sql.Connection;
 
 public class Main {
     public  static void main(String[] args) {
-        try {
+       /* try {
             Connection connection = new DataConnection().getConnection();
             if(connection != null) {
                 System.out.println("Connection OK");
             }
+        }catch (Exception ex) {
+            System.out.println(ex);
+        }*/
+
+        Contact contact = new Contact("toto", "tata");
+        try {
+            ContactDAO dao = new ContactDAO();
+            dao.save(contact);
+            System.out.println(contact.getId());
         }catch (Exception ex) {
             System.out.println(ex);
         }
--- coursJDBC/src/main/java/dao/ContactDAO.java
+++ coursJDBC/src/main/java/dao/ContactDAO.java
@@ -3,6 +3,8 @@ package dao;
 import models.Contact;
 import tools.DataConnection;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
 
 public class ContactDAO extends BaseDao<Contact> {
@@ -10,10 +12,16 @@ public class ContactDAO extends BaseDao<Contact> {
     public Contact save(Contact element) throws Exception{
         requestString = "INSERT INTO contact (nom, prenom) values (?, ?)";
         connection = new DataConnection().getConnection();
-        prepared = connection.prepareStatement(requestString);
+        prepared = connection.prepareStatement(requestString, Statement.RETURN_GENERATED_KEYS);
         prepared.setString(1, element.getNom());
         prepared.setString(2, element.getPrenom());
         prepared.executeUpdate();
+        ResultSet res = prepared.getGeneratedKeys();
+        if(res.next()) {
+            element.setId(res.getInt(1));
+        }
+        prepared.close();
+        connection.close();
         return element;
     }
 
