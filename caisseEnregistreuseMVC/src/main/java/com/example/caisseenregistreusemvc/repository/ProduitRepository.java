905b180ad04f9c84aee50666ff9042e00cffb86d
ihab <ihab@utopios.net>
2022-02-18 11:28:24 +0100

    debut structure avec spring boot et spring data

--- caisseEnregistreuseMVC/src/main/java/com/example/caisseenregistreusemvc/repository/ProduitRepository.java
+++ caisseEnregistreuseMVC/src/main/java/com/example/caisseenregistreusemvc/repository/ProduitRepository.java
@@ -1,10 +1,14 @@
 package com.example.caisseenregistreusemvc.repository;
 
 import com.example.caisseenregistreusemvc.model.Produit;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ProduitRepository extends CrudRepository<Produit, Integer> {
     List<Produit> findByTitre(String titre);
+
+    @Query("from Produit p where p.numero =:id")
+    List<Produit> veryCustomMethod(int id);
 }
